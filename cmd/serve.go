4dae3f1608abd9be9d9cf11ba8c526a13b8353fd
pman <pman-$-432894328923@gmail.com>
2024-10-28 11:51:02 +0100

    chore: update file(s): serve.go

--- cmd/serve.go
+++ cmd/serve.go
@@ -25,16 +25,14 @@ to quickly create a Cobra application.`,
 	},
 }
 
+type serveFlags struct {
+  name string
+}
+
+var serveArgs = serveFlags{}
+
 func init() {
 	rootCmd.AddCommand(serveCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+  serveCmd.Flags().StringVarP(&serveArgs.name, "name", "n", "", "name of the service")
 }
