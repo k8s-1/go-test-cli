278c7a8155c8fcf39290faa4e5e2e4a1fd75a1aa
pman <pman-$-432894328923@gmail.com>
2024-10-28 11:51:54 +0100

    chore: update file(s): serve.go

--- cmd/serve.go
+++ cmd/serve.go
@@ -13,13 +13,12 @@ import (
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Serve a service",
+	Long: `
+Serving a service...
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+e.g. 
+go-test-cli serve name=example`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("serve called")
 	},
