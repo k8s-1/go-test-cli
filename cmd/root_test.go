a31d7b195f3c11de2c6015d5690b30e0f40f5cb5
pman <pman-$-432894328923@gmail.com>
2024-10-28 12:10:25 +0100

    chore: update file(s): root_test.go

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,10 +1,24 @@
 package cmd
 
 import (
-  "testing"
+	"testing"
+  "bytes"
+  "io"
 )
 
 func Test_ExecuteCommand(t *testing.T) {
-	cmd := NewRootCmd("hi")
+	cmd := NewRootCmd()
+	b := bytes.NewBufferString("")
+
+	cmd.SetOut(b)
+	cmd.SetArgs([]string{"serve", "name", "jayquellin"})
 	cmd.Execute()
+
+	out, err := io.ReadAll(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(out) != "hi-via-args" {
+		t.Fatalf("expected \"%s\" got \"%s\"", "hi-via-args", string(out))
+	}
 }
